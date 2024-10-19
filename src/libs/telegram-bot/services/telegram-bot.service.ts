a9c1309a553d4d7f9a3621d2742885538711d5c8
sasha <sasha@profi.io>
2024-10-19 11:34:01 +0700

    add environment

--- src/libs/telegram-bot/services/telegram-bot.service.ts
+++ src/libs/telegram-bot/services/telegram-bot.service.ts
@@ -2,13 +2,21 @@ import { Telegraf } from 'telegraf';
 import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 import { TelegramConfiguration } from '@libs/telegram-bot/config';
+import { SecretsService } from '@libs/aws/secrets.service';
 
 @Injectable()
 export class TelegramBotService {
-  public readonly bot!: Telegraf;
+  public bot!: Telegraf;
 
-  constructor(configService: ConfigService<TelegramConfiguration>) {
-    const token = configService.get<string>('botToken', '');
+  constructor(
+    private configService: ConfigService<TelegramConfiguration>,
+    private readonly secretsService: SecretsService,
+  ) {}
+
+  async init() {
+    // const token = this.configService.get<string>('botToken', '');
+    const token = await this.secretsService.getSecret('prod/TELEGRAM_BOT_TOKEN');
+
     this.bot = new Telegraf(token);
   }
 }
