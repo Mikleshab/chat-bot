0ba30a1c0142cabc3321b36dd2ad3e06f73173ce
sasha <sasha@profi.io>
2024-10-20 17:01:05 +0700

    add auth feature

--- src/core/auth/auth.guard.ts
+++ src/core/auth/auth.guard.ts
@@ -1,25 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { OAuth2Client } from 'google-auth-library';
 import { ConfigService } from '@nestjs/config';
 import { IncomingMessage } from 'node:http';
-import { TokenPayload } from 'google-auth-library/build/src/auth/loginticket';
 import { AuthConfig } from './config/config.schema';
+import * as admin from 'firebase-admin';
+import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  private readonly googleClientId: string;
   private readonly allowedEmail: string;
-  private client: OAuth2Client;
 
   constructor(configService: ConfigService<AuthConfig>) {
     this.allowedEmail = configService.get<string>('adminEmail', '');
-    this.googleClientId = configService.get<string>('googleClientId', '');
-    this.client = new OAuth2Client(this.googleClientId);
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const ctx = GqlExecutionContext.create(context).getContext<{ req: IncomingMessage; user?: TokenPayload }>();
+    const ctx = GqlExecutionContext.create(context).getContext<{ req: IncomingMessage; user?: DecodedIdToken }>();
     const { token } = ctx.req.headers;
 
     if (!token) {
@@ -27,13 +23,8 @@ export class AuthGuard implements CanActivate {
     }
 
     try {
-      const ticket = await this.client.verifyIdToken({
-        idToken: token as string,
-        audience: this.googleClientId,
-      });
-
-      ctx.user = ticket.getPayload();
-    } catch (error: unknown) {
+      ctx.user = await admin.auth().verifyIdToken(token as string);
+    } catch {
       throw new UnauthorizedException('Invalid token.');
     }
 
