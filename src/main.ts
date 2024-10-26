b9037958eab2d62dc856e5d88a1a2e9c860bf6dc
sasha <sasha@profi.io>
2024-10-26 08:42:48 +0700

    add auth feature

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,14 @@ import { CommonConfiguration, DEFAULT_PORT } from '@config/common.config';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { snapshot: true });
   app.enableCors({
-    origin: 'http://localhost:4200', // Замените на URL вашего фронтенда
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Token',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService<CommonConfiguration>);
-  await app.listen(configService.get('port', DEFAULT_PORT));
+  await app.listen(configService.get('port', DEFAULT_PORT), '0.0.0.0');
 }
 
 bootstrap();
