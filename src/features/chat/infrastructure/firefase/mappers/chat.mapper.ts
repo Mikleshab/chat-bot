2d1d703b337372003c8cded30198fd1e02c96bf3
sasha <sasha@profi.io>
2024-10-28 12:27:33 +0700

    (bugfix) fix ChatMapper title -> name

--- src/features/chat/infrastructure/firefase/mappers/chat.mapper.ts
+++ src/features/chat/infrastructure/firefase/mappers/chat.mapper.ts
@@ -2,6 +2,6 @@ import { Chat } from '@features/chat/domain/models/chat';
 
 export class ChatMapper {
   static toDomain(doc: any): Chat {
-    return new Chat(doc.id, doc.title);
+    return new Chat(doc.id, doc.name);
   }
 }
